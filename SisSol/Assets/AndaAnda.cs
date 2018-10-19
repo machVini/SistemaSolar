5eea5089a63672f710cf0b734c8ecc35da6c4bc5
Vinicius Machado <viniciusmach07@gmail.com>
2018-10-19 20:24:42 -0300

    "Almost" Commit

---
+++ SisSol/Assets/AndaAnda.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AndaAnda : MonoBehaviour {
+
+  public Vector3 movimento;
+  void Start ()
+  {
+		
+	}
+
+	void Update ()
+  {
+    Vector3 translacao = new Vector3(movimento.x * Time.deltaTime,
+                                     movimento.y * Time.deltaTime,
+                                     movimento.z * Time.deltaTime);
+    transform.Translate(translacao);
+	}
+}
---
+++ SisSol/Assets/GiraGira.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GiraGira : MonoBehaviour {
+
+  Vector3 rotacao;
+
+  public float rotacaoX, rotacaoY, rotacaoZ;
+
+	// Use this for initialization
+	void Start ()
+  {
+    
+	}
+	
+	// Update is called once per frame
+	void Update ()
+  {
+    rotacao = new Vector3(rotacaoX * Time.deltaTime, 
+                          rotacaoY * Time.deltaTime,
+                          rotacaoZ * Time.deltaTime 
+                         );
+    transform.Rotate(rotacao);
+	}
+}
